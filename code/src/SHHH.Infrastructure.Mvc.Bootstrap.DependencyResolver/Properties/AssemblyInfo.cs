64786e886f48e8dd863ca5fb0bf8f896443fb492
ghorsey <geoff@shhhinnovations.com>
2012-09-01 13:15:00 -0700

    added dependency resolver

--- code/src/SHHH.Infrastructure.Mvc.Bootstrap.DependencyResolver/Properties/AssemblyInfo.cs
+++ code/src/SHHH.Infrastructure.Mvc.Bootstrap.DependencyResolver/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SHHH.Infrastructure.Mvc.Bootstrap.Ninject")]
+[assembly: AssemblyTitle("SHHH.Infrastructure.Bootstrap.DependencyResolver")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SHHH.Infrastructure.Mvc.Bootstrap.Ninject")]
+[assembly: AssemblyProduct("SHHH.Infrastructure.Bootstrap.DependencyResolver")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d5bc0855-21da-495d-a9a7-a816440273c2")]
+[assembly: Guid("2b5c63d7-5a4f-4c82-8c55-f957066f4748")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
