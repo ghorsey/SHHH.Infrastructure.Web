e01bdc6c14c9a308513087cb5cb3d4d13a1e52aa
ghorsey <geoff@shhhinnovations.com>
2012-11-14 10:25:11 -0800

    made reflectionhelper away of the action name attribute

--- code/src/SHHH.Infrastructure.Web.Testing/ReflectionHelper.cs
+++ code/src/SHHH.Infrastructure.Web.Testing/ReflectionHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace SHHH.Infrastructure.Web.Testing
 {
@@ -15,9 +16,15 @@ namespace SHHH.Infrastructure.Web.Testing
         public static string GetMethodName<T, U>(Expression<Func<T, U>> expression)
         {
             var method = expression.Body as MethodCallExpression;
-            if (method != null)
-                return method.Method.Name;
-            throw new ArgumentException("Expression is wrong");
+            if (method == null) throw new ArgumentException("Expression is wrong");
+
+            var attr = method.Method.CustomAttributes.Where(x => typeof(ActionNameAttribute).IsAssignableFrom(x.AttributeType)).FirstOrDefault();
+            
+            if (attr != null && attr.ConstructorArguments.Count == 1)
+                return (string)attr.ConstructorArguments[0].Value;
+                
+
+            return method.Method.Name;
         }
     }
 }
---
+++ code/src/SHHH.Infrastructure.Web.Tests/ReflectionHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NUnit.Framework;
+using SHHH.Infrastructure.Web.Testing;
+
+namespace SHHH.Infractructure.Web.Tests
+{
+    [TestFixture]
+    public class ReflectionHelperTests
+    {
+        [ActionName("MyName")]
+        public string OtherName() { return ""; }
+
+        public string NonAttribute() { return ""; }
+
+        [Test]
+        public void GetMethodName_Test()
+        {
+            var myName = ReflectionHelper.GetMethodName((ReflectionHelperTests rht) => rht.OtherName());
+            var nonAttribute = ReflectionHelper.GetMethodName((ReflectionHelperTests rht) => rht.NonAttribute());
+
+            Assert.AreEqual("NonAttribute", nonAttribute);
+            Assert.AreEqual("MyName", myName);
+        }
+    }
+}
