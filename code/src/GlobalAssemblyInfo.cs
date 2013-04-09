330e7bfbf96818688fd72137cf6149e05fc31934
ghorsey <geoff@shhhinnovations.com>
2013-04-09 01:28:37 -0700

    marked assemblies as CLS compliant and to allow partially trusted callers

--- code/src/GlobalAssemblyInfo.cs
+++ code/src/GlobalAssemblyInfo.cs
@@ -1,10 +1,16 @@
 // <copyright file="GlobalAssemblyInfo.cs" company="SHHH Innovations LLC">
 // Copyright SHHH Innovations LLC
 // </copyright>
+
+using System;
 using System.Reflection;
+using System.Security;
 
 [assembly: AssemblyCompany("SHHH Innovations LLC")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.3.3.1")]
-[assembly: AssemblyFileVersion("1.3.3.1")]
+[assembly: AssemblyVersion("1.3.3.2")]
+[assembly: AssemblyFileVersion("1.3.3.2")]
+
+[assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
